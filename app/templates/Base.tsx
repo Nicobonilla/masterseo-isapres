c9b15c749bd4013acfb88681652dfa3833f512eb
Nicolas Bonilla <n.bonillamarin@gmail.com>
2024-01-22 18:18:58 -0300

    Se crea un layout base con la barra de navegacion y ajustando margenes x

--- app/templates/Base.tsx
+++ app/templates/Base.tsx
@@ -1,17 +1,55 @@
 import React from 'react';
 import { Meta } from '../components/layout/Meta';
 import { AppConfig } from '../components/utils/AppConfig';
+import { Background } from '../components/background/Background';
+import { Section } from '../components/layout/Section';
+import { NavbarTwoColumns } from '../components/navigation/NavbarTwoColumns';
+import { Logo } from './Logo';
 import { Banner } from './Banner';
 import { Hero } from './Hero';
 import { Footer } from './Footer';
+import Link from 'next/link';
 
-export default function Base() {
+import type { ReactNode } from 'react';
+
+type IBaseProps = {
+  children: ReactNode;
+};
+
+const Base = (props: IBaseProps) => {
   const { title, description } = AppConfig;
-  return (<>
-    <Meta title={title} description={description} />
-    <Hero />
-    <Banner />
-    <Footer />
-  </>
+  return (
+    <>
+      <Meta title={title} description={description} />
+      <Background color="bg-background text-foreground">
+        <Section yPadding="py-6">
+          <NavbarTwoColumns logo={<Logo xl />}>
+            <li className='sm:pr-3 lg:pr-6 xl:pr-12'>
+              <Link href="https://github.com/ixartz/Next-JS-Landing-Page-Starter-Template">
+                Nosotros
+              </Link>
+            </li>
+            <li className='sm:pr-3 lg:pr-6 xl:pr-12'>
+              <Link href="https://github.com/ixartz/Next-JS-Landing-Page-Starter-Template">
+                Planes
+              </Link>
+            </li>
+            <li className='sm:pr-3 lg:pr-6 xl:pr-12'>
+              <Link href="/informacion-isapres-chile">
+                Boletín
+              </Link>
+            </li>
+            <li>
+              <Link href="/">Cotizar</Link>
+            </li>
+          </NavbarTwoColumns>
+        </Section>
+        {props.children}
+      </Background>
+      <Banner />
+      <Footer />
+    </>
   );
 }
+
+export { Base }
