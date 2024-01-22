503f43ff21efeb085e6c26f5823b9890eb2cba86
Nicolas Bonilla <n.bonillamarin@gmail.com>
2024-01-22 17:59:38 -0300

    It's been given values to variables related to components of NextJS material tailwind kit start templates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
 const withMT = require("@material-tailwind/react/utils/withMT");
 module.exports = withMT({
   darkMode: ["class"],
@@ -73,6 +74,53 @@ module.exports = withMT({
           DEFAULT: "hsl(var(--card))",
           foreground: "hsl(var(--card-foreground))",
         },
+        fuchsia: colors.fuchsia,
+        cyan: colors.cyan,
+        emerald: colors.emerald,
+        teal: colors.teal,
+        orange: colors.orange,
+        yellow: colors.yellows,
+      },
+      maxHeight: {
+        116: "29rem",
+        /*464px*/
+        125: "31.25rem",
+        /*500px*/
+        140: "35rem",
+        /*560px*/
+        150: "37.5rem",
+        /*600px*/
+        190: "47.5rem",
+        /*760px*/
+      },
+      height: {
+        116: "29rem",
+        /*464px*/
+        125: "31.25rem",
+        /*500px*/
+        140: "35rem",
+        /*560px*/
+        150: "37.5rem",
+        /*600px*/
+        200: "50rem",
+        /*800px*/
+        250: "62.5rem",
+        /*800px*/
+      },
+      minHeight: {
+        18: "4.5rem",
+        /*72*/
+        33: "8.125rem",
+        /*132px*/
+        62: "15.5rem",
+        /*248px*/
+        96: "24rem",
+        /*384px*/
+        150: "37.5rem",
+        /*600px*/
+      },
+      margin: {
+        '-100%': '-100%'
       },
       lineHeight: {
         hero: '4.5rem',
@@ -96,6 +144,19 @@ module.exports = withMT({
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+    },
+  },
+  variants: {
+    extend: {
+      display: ["group-hover"],
+      opacity: ["group-hover"],
+      translate: ["group-hover"],
+      transform: ["group-hover"],
+      width: ["group-hover", "hover"],
+      height: ["group-hover", "hover"],
+      padding: ["group-hover", "hover"],
+      animation: ["group-hover", "hover"],
+      scale: ["group-hover", "hover"],
     },
   },
   plugins: [require("tailwindcss-animate")],
