a9113fcf9b40b5233423ecba4c56cef85aaa3f2a
Nicolas Bonilla <n.bonillamarin@gmail.com>
2024-01-23 17:39:06 -0300

    Se eliminan los enlaces en top-nav

--- app/components/navigation/NavbarTwoColumns.tsx
+++ app/components/navigation/NavbarTwoColumns.tsx
@@ -9,16 +9,8 @@ type INavbarProps = {
 };
 
 const NavbarTwoColumns = (props: INavbarProps) => (
-  <div className="flex flex-wrap items-center justify-between">
-    <div>
+  <div className="flex flex-wrap items-center justify-center">
       <Link href="/">{props.logo}</Link>
-    </div>
-
-    <nav>
-      <ul className="navbar flex items-center text-xl font-medium text-gray-800">
-        {props.children}
-      </ul>
-    </nav>
   </div>
 );
 
