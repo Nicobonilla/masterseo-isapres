2281c053306eef107a2c67acaa3f82e0f97cb84f
Nicolas Bonilla <n.bonillamarin@gmail.com>
2024-01-29 12:19:00 -0300

    Backend envío de datos de formulario  vía email

---
+++ app/api/email/route.tsx
@@ -0,0 +1,80 @@
+import { type NextRequest, NextResponse } from "next/server";
+import nodemailer from "nodemailer";
+import { render } from "@react-email/render";
+import { Email } from "../../templates/Email";
+
+export async function POST(request: NextRequest) {
+  const {
+    nombre,
+    rut,
+    edad,
+    celular,
+    comuna,
+    email,
+    rentaImponible,
+    tipoPlan,
+    cantidadCargas,
+    comentario,
+  } = await request.json();
+
+  const emailHtml = render(
+    <Email
+      nombre={nombre}
+      rut={rut}
+      edad={edad}
+      celular={celular}
+      comuna={comuna}
+      email={email}
+      rentaImponible={rentaImponible}
+      tipoPlan={tipoPlan}
+      cantidadCargas={cantidadCargas}
+      comentario={comentario}
+    />
+  );
+
+  const my_email = process.env.MY_EMAIL;
+ 
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: my_email,
+      pass: process.env.MY_PASSWORD_APP,
+    },
+  });
+
+  try {
+    await new Promise((resolve, reject) => {
+      transporter.verify(function (error, success) {
+        if (error) {
+          console.error(error);
+          reject(error);
+        } else {
+          console.log("Server is ready to take our messages");
+          resolve(success);
+        }
+      });
+    });
+
+    const mailOptions = {
+      from: my_email,
+      to: my_email,
+      subject: `Red Isapres: ${nombre} RI: ${rentaImponible}`,
+      html: emailHtml,
+    };
+    
+    await new Promise((resolve, reject) => {
+      // send mail
+      transporter.sendMail(mailOptions, (err, mailres) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(mailres);
+        }
+      });
+    });
+    return NextResponse.json( {status: 200 });
+  } catch ( error) {
+    console.error(error); // Registra el error en la consola
+    return NextResponse.json({ status: 500});
+  }
+}
---
+++ app/templates/Email.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import { Html } from '@react-email/html';
+
+interface EmailProps {
+    nombre: string;
+    rut: string;
+    edad: number;
+    celular: string;
+    comuna: string;
+    email: string;
+    rentaImponible: number;
+    tipoPlan: string;
+    cantidadCargas: number;
+    comentario: string;
+  }
+
+export function Email(props: EmailProps) {
+  const { nombre, rut, edad, celular, comuna, email, rentaImponible, tipoPlan, cantidadCargas, comentario } = props;
+
+  return (
+    <Html lang="es">
+      <h1>Detalle de Información</h1>
+      <p>Nombre: {nombre}</p>
+      <p>RUT: {rut}</p>
+      <p>Edad: {edad}</p>
+      <p>Celular: {celular}</p>
+      <p>Comuna: {comuna}</p>
+      <p>Email: {email}</p>
+      <p>Renta Imponible: {rentaImponible}</p>
+      <p>Tipo de Plan: {tipoPlan}</p>
+      <p>Cantidad de Cargas: {cantidadCargas}</p>
+      <p>Comentario: {comentario}</p>
+    </Html>
+  );
+}
