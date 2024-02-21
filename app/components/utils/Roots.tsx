50405a18aa9aa6dabf72f166cfcc0b19870ea22c
Nicolas Bonilla <n.bonillamarin@gmail.com>
2024-02-21 10:24:51 -0300

    Fix throw error for undefined url_localhost'

--- app/components/utils/Roots.tsx
+++ app/components/utils/Roots.tsx
@@ -2,12 +2,10 @@ export default function Roots() {
   const { ROOT_URL_HOST, ROOT_URL_LOCALHOST } = process.env;
   // Runtime check to ensure ROOT_URL_HOST is not undefined
   if (typeof ROOT_URL_HOST === "undefined") {
-    throw new Error("ROOT_URL_HOST is not defined");
+    if (typeof ROOT_URL_LOCALHOST === "undefined") {
+      throw new Error("ROOT_URL_LOCALHOST is not defined");
+    }
+    const root = ROOT_URL_HOST ? ROOT_URL_HOST : ROOT_URL_LOCALHOST;
+    return root;
   }
-  // Runtime check to ensure ROOT_URL_HOST is not undefined
-  if (typeof ROOT_URL_LOCALHOST === "undefined") {
-    throw new Error("ROOT_URL_LOCALHOST is not defined");
-  }
-  const root = ROOT_URL_HOST ? ROOT_URL_HOST : ROOT_URL_LOCALHOST;
-  return root;
 }
